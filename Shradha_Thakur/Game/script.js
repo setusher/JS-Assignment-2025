3a87f0d00ed1e13a536619cbed2f7fd92f1ab292
setusher <shradhath08@gmail.com>
2025-07-02 11:04:56 +0530

    gameover and score logic done, added images

--- Shradha_Thakur/Game/script.js
+++ Shradha_Thakur/Game/script.js
@@ -16,10 +16,25 @@ CanvasRenderingContext2D.prototype.fillCircle = function(x, y, r) {
   
 const GRAVITY = 0.5;    
 const JUMP_STRENGTH = -15;
-const GAME_SPEED = 5;
+let GAME_SPEED = 7;
+ 
 
-const GAME_WIDTH = 800;
+
+const GAME_WIDTH = 1000;
 const GAME_HEIGHT = 600;
+
+const playerImg = new Image();
+playerImg.src = 'assets/player.png';
+
+const platformImg = new Image();
+platformImg.src = 'assets/platform.avif';
+
+const obstacleImg = new Image();
+obstacleImg.src = 'assets/obstacle.png';
+
+const coinImg = new Image();
+coinImg.src = 'assets/coin.png';
+
 
 class Player {
     constructor(x, y, width, height) {
@@ -40,8 +55,8 @@ class Player {
         }
     }
     draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        
+        ctx.drawImage(playerImg,this.x, this.y, this.width, this.height);
     }
 
     jump(){
@@ -62,8 +77,8 @@ class Platform {
         this.x -= GAME_SPEED;
     }
     draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(platformImg, this.x, this.y, this.width, this.height);
+
     }
     isOffScreen(){
         return this.x + this.width < 0;
@@ -84,8 +99,8 @@ class Obstacle {
     }
 
     draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        
+        ctx.drawImage(obstacleImg, this.x, this.y, this.width, this.height);
     }
     isOffScreen(){
         return this.x + this.width < 0;
@@ -100,20 +115,28 @@ class Ring{
         this.height = height;
         this.color = 'yellow';
     }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillCircle(this.x, this.y, this.width);
+    update(){
+        this.x -= GAME_SPEED;
     }
+
+    draw() {
+   
+        ctx.drawImage(coinImg, this.x, this.y, this.width, this.height);
+    }
+    isOffScreen(){
+        return this.x + this.width < 0;
+    }
+
 }
 
-const pf_height= 50;
-const pf_y = GAME_HEIGHT - pf_height;
-const pf_width = 100;
+const pf_height= 800;
+const pf_y = 0;
+const pf_width = 1000;
 
 let platforms = [];
 
 function initGround(){
-    for (let i = 0; i < GAME_WIDTH/pf_width +2 ; i++){
+    for (let i = 0; i <= Math.ceil(GAME_WIDTH / pf_width) + 6 ; i++){
         platforms.push(new Platform(i*pf_width, pf_y, pf_width, pf_height));
     }   
 
@@ -128,6 +151,7 @@ function updateGround(){
     }
 
     for (let i = 0; i < platforms.length; i++){
+        // GAME_SPEED += 0.00002;
         platforms[i].update();
     }
 
@@ -144,19 +168,31 @@ function updateGround(){
 }
 
 let obstacles = [];
-const player = new Player(50,150,30,30);
+const player = new Player(50,150,80,100);
+let collectables = [];
+let score = 0;
+
 
 function update(){
     updateGround();
 
+    for(let i = 0; i < obstacles.length; i++){
+       
+        if(isColliding(player, obstacles[i])){
+            console.log("Game Over");
+            gameover();
+            break;
+        }
+    }
+
     if(Math.random()<0.5 && obstacles.length < 5){
 
-        const obstacleWidth = 20;
-        const obstacleHeight = 40;
-        const groundY = GAME_HEIGHT - pf_height - obstacleHeight;
+        const obstacleWidth = 50;
+        const obstacleHeight = 80;
+        const groundY = 520;
         
         if (Math.random() < 0.02 && obstacles.length < 3) {
-          obstacles.push(new Obstacle(GAME_WIDTH, groundY, obstacleWidth, obstacleHeight));
+          obstacles.push(new Obstacle(GAME_WIDTH+700, groundY, obstacleWidth, obstacleHeight));
         }
   
     }
@@ -169,6 +205,32 @@ function update(){
         }   
     }
 
+    if(Math.random()<0.2 && collectables.length < 5){
+
+        const ringWidth = 100;
+        const ringHeight = 100;
+        const groundY = 520;
+        
+        if (Math.random() < 0.02 && collectables.length < 3) {
+          collectables.push(new Ring(GAME_WIDTH+700, groundY, ringWidth, ringHeight));
+        }
+  
+    }
+
+    for(let i = collectables.length-1; i>=0; i--){
+        collectables[i].update();
+
+        if(collectables[i].isOffScreen()){
+            collectables.splice(i, 1);
+            continue;
+        }   
+
+        if(isColliding(player, collectables[i])){
+            score += 1;
+            collectables.splice(i, 1);
+        }
+    }
+
     player.update();
     
 }
@@ -177,9 +239,30 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let pf of platforms) pf.draw();
     for (let ob of obstacles) ob.draw();
+    for (let c of collectables) c.draw();
+
     player.draw();
+
+    ctx.fillStyle = 'black padding 20px';
+    ctx.font = '40px Arial';
+    ctx.fillText("Score: " + score, 20, 40);
+
 }
 
+function isColliding(r1,r2){
+    return (
+        r1.x<r2.x+r2.width &&  
+        r1.x+r1.width>r2.x &&
+        r1.y<r2.y+r2.height &&
+        r1.y+r1.height>r2.y
+    );
+}
+
+function gameover(){
+    alert("Game Over! Your Score is: " + score);
+
+    location.document.reload();
+}
 
 
 initGround();
